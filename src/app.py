89c20e10e3ad048b2ca9e8e319befbacf5152efa
kellanliu188 <kellanliu188@gmail.com>
2024-04-28 20:57:37 -0400

    add delete end point

--- src/app.py
+++ src/app.py
@@ -37,6 +37,7 @@ def delete_a_ride(ride_id):
 
     ride = Rides.query.filter_by(id = ride_id).first()
 
+
     if ride is None:
         return failure_response("Ride not found")
     db.session.delete(ride)
